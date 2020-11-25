8f02cefa24329a07e2658d5ee8d9e413e9ecaaee
MasenkoPC <MasenkoPC@192.168.1.146>
2020-11-26 00:29:24 +0100

    Creados setters y getters de clase posicion, validados y excepcion

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
@@ -7,4 +7,30 @@ public class Posicion
 	private char columna;
 	
 	
+	//metodos
+	
+	//getters y setters
+	public int getFila() {
+		return fila;
+	}
+	private void setFila(int fila) {
+		if(fila<1 || fila>8)
+			throw new IllegalArgumentException("ERROR: fila no válida");
+		
+		this.fila = fila;
+	}
+	public char getColumna() {
+		return columna;
+	}
+	private void setColumna(char columna) {
+		if(columna<'a' || columna>'h')
+			throw new IllegalArgumentException("ERROR: columna no válida");
+		
+		this.columna = columna;
+	}
+	
 }
+
+//6.Crea los métodos get y set para los atributos. Recuerda que para el método set se debe tener en cuenta que los valores
+//son correctos (las filas van del 1 al 8 -ambos inclusive- y las columnas de la 'a' a la 'h' -ambas inclusive-) y si no se 
+//lance una excepción del tipo IllegalArgumentException con el mensaje adecuado y no modifique los atributos. Realiza un commit
