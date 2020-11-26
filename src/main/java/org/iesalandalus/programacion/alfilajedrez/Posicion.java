e7d9a617ab2a56b19d4ea9e833b162bd9c981294
MasenkoPC <MasenkoPC@192.168.1.146>
2020-11-26 01:30:51 +0100

    Creados métodos equals y hashcode en clase Posicion

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
@@ -44,8 +44,33 @@ public class Posicion
 		
 		this.columna = columna;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + columna;
+		result = prime * result + fila;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Posicion other = (Posicion) obj;
+		if (columna != other.columna)
+			return false;
+		if (fila != other.fila)
+			return false;
+		return true;
+	}
+	
+	
 	
 }
-
-//8.Crea el constructor copia para esta clase. Realiza un commit.
 
