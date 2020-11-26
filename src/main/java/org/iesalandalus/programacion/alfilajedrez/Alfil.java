cdb233b906dac0edf943962adae9b94ee979a71e
MasenkoPC <MasenkoPC@192.168.1.146>
2020-11-26 14:06:31 +0100

    Proyecto abierto de nuevo

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
@@ -5,7 +5,30 @@ public class Alfil
 	//atributos
 	private Color color;
 	private Posicion posicion;
+	
+	//métodos
+	
+	//getters y setters
+	public Color getColor() {
+		return color;
+	}
+	private void setColor(Color color) {
+		if(color==null)
+			throw new NullPointerException("ERROR: el color no puede ser nulo");
+		else
+			this.color = color;
+	}
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	private void setPosicion(Posicion posicion) {
+		if(posicion==null)
+			throw new NullPointerException("ERROR: la posicion no es valida");
+		else
+			this.posicion = new Posicion(posicion);
+	
+	}
 }
 
-//11.Crea la clase Alfil, dentro del paquete adecuado, cuyos atributos serán un color (del tipo enumerado Color)
-//y posicion (de la clase Posicion), con la visibilidad adecuada. Realiza un commit.
+//12.Crea los métodos get y set para cada atributo con la visibilidad adecuada. Estos métodos siempre comprobarán la validez de los parámetros pasados 
+//y si no son correctos deberá lanzar la excepción adecuada (NullPointerException o IllegalArgumentException) con el mensaje adecuado. Realiza un commit.
