8cbf5b2058d81c060ab853fcc8a4ec3f6d56cfd5
MasenkoPC <MasenkoPC@192.168.1.146>
2020-11-26 14:37:48 +0100

    Creado método mostrarAlfil

---
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
@@ -0,0 +1,19 @@
+package org.iesalandalus.programacion.alfilajedrez;
+
+public class MainApp {
+
+	public static void main(String[] args) 
+	{
+
+	}
+	
+	//métodos
+	
+	private static Alfil alfil;
+	
+	private static void mostrarAlfil() 
+	{
+		System.out.println(alfil);
+	}
+
+}
