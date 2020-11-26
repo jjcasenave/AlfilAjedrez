57dbc15c95c21721e58804fe16dd19b16541de07
MasenkoPC <MasenkoPC@192.168.1.146>
2020-11-26 14:15:57 +0100

    constructor con parametros color y columna inicial en clase alfil

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
@@ -28,6 +28,20 @@ public class Alfil
 			posicion=new Posicion(8, 'f');
 	}
 	
+	public Alfil(Color color, char columna) throws NullPointerException, IllegalArgumentException
+	{
+		if(columna!='c' || columna!='f')
+			throw new IllegalArgumentException("ERROR: la columna tiene que ser c o f");
+		if(color==null)
+			throw new NullPointerException("ERROR: el color no puede ser nulo");
+		
+		setColor(color);
+		if(color.equals(Color.BLANCO))
+			posicion=new Posicion(1, columna);
+		else
+			posicion=new Posicion(8, columna);
+	}
+	
 	//getters y setters
 	public Color getColor() {
 		return color;
@@ -50,6 +64,9 @@ public class Alfil
 	}
 }
 
-//14.Crea un constructor para la clase que acepte como parámetro el color y que creará un alfil de 
-//dicho color cuya posición será '1f' si es blanco o '8f' si es negro. Realiza un commit.
+//15.Crea un constructor para la clase que acepte como parámetros el color y la columna inicial. La columna inicial debe ser válida 
+//(de lo contrario debe lanzar la excepción IllegalArgumentException con un mensaje adecuado) y creara un alfil del color dado y 
+//colocado en dicha columna ('c' o 'f') y cuya fila será la 1 si es blanco y la 8 si es negro. Realiza un commit
+
+
 
