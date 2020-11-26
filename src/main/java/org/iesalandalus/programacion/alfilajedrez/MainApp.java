09f074849cdab10f5fa0fcb3c873dbda35d2003d
MasenkoPC <MasenkoPC@192.168.1.146>
2020-11-26 14:38:26 +0100

    Creado método mostrarMenu en clase principal

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
@@ -15,5 +15,18 @@ public class MainApp {
 	{
 		System.out.println(alfil);
 	}
+	
+	private static void mostrarMenu() 
+	{
+		System.out.println("Opciones");
+		System.out.println("-----------------------------------");
+		System.out.println("1.Crear un alfil por defecto");
+		System.out.println("2.Crear un alfil de un color");
+		System.out.println("3.Crear un alfil de un color con columna inicial");
+		System.out.println("4.Mover un alfil");
+		System.out.println("5.Salir");
+	}
+	
+	
 
 }
